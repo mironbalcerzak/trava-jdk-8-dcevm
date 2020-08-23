e9049ddac4661b7605e1b4529664267a3a83ba88
Miron Balcerzak <mironbalcerzak@gmail.com>
2020-08-24 00:25:48 +0200

    jdk version specified

--- build_windows.sh
+++ build_windows.sh
@@ -23,4 +23,4 @@ export LOG=info
 export LANG=C
 export JAVA_HOME=$JDK_BOOT_DIR
 
-./makejdk-any-platform.sh --repository "https://github.com/mironbalcerzak/openjdk-jdk8u-dcevm" --branch "${SOURCE_JDK_BRANCH}" --tag "${SOURCE_JDK_TAG}" --jdk-boot-dir "${JDK_BOOT_DIR}" --build-variant dcevm --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --check-fingerprint false --configure-args "--with-freetype-include=${FREETYPE_DIR}/include --with-freetype-lib=${FREETYPE_DIR}/win64 --disable-ccache" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u
+./makejdk-any-platform.sh --repository "https://github.com/mironbalcerzak/openjdk-jdk8u-dcevm" --update-version 232 --build-number b09 --branch "${SOURCE_JDK_BRANCH}" --tag "${SOURCE_JDK_TAG}" --jdk-boot-dir "${JDK_BOOT_DIR}" --build-variant dcevm --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --check-fingerprint false --configure-args "--with-freetype-include=${FREETYPE_DIR}/include --with-freetype-lib=${FREETYPE_DIR}/win64 --disable-ccache" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u
